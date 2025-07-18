5c557afa0c89e311ec6690b4e574817664be98df
DevAlexSanz <devalexsanz@gmail.com>
2025-07-17 23:55:35 -0600

    fix: logo

--- src/views/solo/layout.tsx
+++ src/views/solo/layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import DavidaLogo from "@/assets/DaVida.png";
 
 export const SoloLayout = () => {
   return (
@@ -7,7 +8,7 @@ export const SoloLayout = () => {
       <div className="absolute w-[400px] h-[300px] bg-[#3DDC97] rounded-full blur-3xl opacity-40 z-0 top-1/2 left-1/2 -translate-x-[40%] -translate-y-1/2" />
 
       <div className="relative z-10 flex flex-col items-center justify-center text-center w-full">
-        <img src="/src/assets/DaVida.png" alt="Description" className="w-40" />
+        <img src={DavidaLogo} alt="Description" className="w-40" />
         <Outlet />
       </div>
     </div>
